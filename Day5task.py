ef8a5d3bbd8ff0996bfac5ba3b68a855fa281a96
abarnaanbu11 <abarnaanbu11@gmail.com>
2025-10-10 17:04:30 +0530

    Add files via upload
    Day5task

---
+++ Day5task.py
@@ -0,0 +1,27 @@
+#forloop
+for i in range(10,0,-1):
+    print(i)
+    
+#while
+n=0
+while n<=5:
+    print (n)
+    n+=1
+    print("Abarna")
+    
+#break
+n=0
+while n<=10:
+    n+=1
+    print(n)
+    if n==5:
+        break
+
+#factorial
+    
+num=(int(input("Enter the value:")))
+fact=1
+for i in range (1,num+1):
+    fact=fact*i
+print(fact)
+   
